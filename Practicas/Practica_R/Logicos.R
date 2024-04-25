cb7db91db69f8180e4bdd9a1416a741776e132f8
Manuel Nava <manueldavidnava31@gmail.com>
2024-04-25 17:12:00 -0400

    Practica Lenguaje R

---
+++ Practicas/Practica_R/Logicos.R
@@ -0,0 +1,27 @@
+#Operadores Logicos
+
+# AND (&)
+TRUE & TRUE
+
+TRUE & FALSE
+
+FALSE & FALSE
+
+#OR (|)
+
+TRUE | TRUE
+
+TRUE|FALSE
+
+FALSE|FALSE
+
+#Asignar una variable
+
+h=2
+h*3
+
+x <- 5
+x%%2
+
+assign("j",30)
+j*3
---
+++ Practicas/Practica_R/Operaciones.R
@@ -0,0 +1,27 @@
+#Operaciones Logicas
+
+#Suma
+
+30+12
+
+#Resta
+
+20-2
+
+#Multiplicacion
+
+20*12
+
+#Division 
+
+30000/1500
+
+#Jerarquia de Operaciones
+
+2*(2+3)
+
+(8*2)/4
+
+#Modulo
+
+15%%4
