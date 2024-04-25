1179421b1c84dbd1da398dd6d2884a786cda306c
Manuel Nava <manueldavidnava31@gmail.com>
2024-04-25 15:56:35 -0400

    Agregando Entorno Virtual con la Practica 2 y Practica en Numpy

---
+++ Practicas/Practica_2/entorno/Prueba_Numpy.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+"""
+    Calculo Numerico - Practica 2 - 23/04/24
+    
+    Manuel Nava
+    30.822.007
+    Seccion: 305C1
+    
+    Practica con la libreria Numpy.
+"""
+
+
+a = np.array([1,2,3])
+b = np.arange(6)
+b.sort
+x=b.reshape(3,2)
+
+c = np.concatenate((a,b))
+print(x)
+print()
+
+print(c)
